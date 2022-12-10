6e082bafdbc0b51f85ae9672a72cac2f53fa3a93
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-10 15:19:53 +0200

    1 - START

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,14 @@
-const URL = 'https://restcountries.com/v3.1';
+// const URL = 'https://restcountries.com/v3.1';
 
-export const fetchCountries = name => {
-  return fetch(
-    `${URL}/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => response.json())
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      return error;
-    });
-};
+// export const fetchCountries = name => {
+//   return fetch(
+//     `${URL}/name/${name}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(response => response.json())
+//     .then(data => {
+//       return data;
+//     })
+//     .catch(error => {
+//       return error;
+//     });
+// };
