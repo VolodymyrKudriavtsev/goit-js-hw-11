913ce23ad27f7fed78684556bebfb6e60822ea96
VolodymyrKudriavtsev <108152577+VolodymyrKudriavtsev@users.noreply.github.com>
2022-12-11 13:38:38 +0000

    Deploying to gh-pages from @ VolodymyrKudriavtsev/goit-js-hw-11@f03ab1bda6d39a6b74dad2805408c9825f0c6e96 🚀

--- index.5e821d58.js
+++
@@ -1,2 +0,0 @@
-var e=refs={};const o=new URLSearchParams({key:"31991210-5d8d315bab6d2995c6cf86716",q:"cats",image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(e),o.set("q","searchBoxValue"),fetch(`https://pixabay.com/api/?${o}`).then((e=>{if(e.ok)return e.json();throw new Error})).then((e=>console.log(e))).catch((e=>e));
-//# sourceMappingURL=index.5e821d58.js.map
