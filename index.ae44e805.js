9699a026afdbe2365d2576df0b637b3b40426c0e
VolodymyrKudriavtsev <108152577+VolodymyrKudriavtsev@users.noreply.github.com>
2022-12-11 17:29:58 +0000

    Deploying to gh-pages from @ VolodymyrKudriavtsev/goit-js-hw-11@b1f140d7d4c69dccf029f87298d42623978a9776 🚀

--- index.ae44e805.js
+++
@@ -1,5 +0,0 @@
-!function(){var t=refs={searchForm:document.querySelector(".search-form"),submitButton:document.querySelector(".let-search")},e=new URLSearchParams({key:"31991210-5d8d315bab6d2995c6cf86716",q:"cats",image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(t);var o="",r=[];console.dir(t.submitButton);t.searchForm.addEventListener("submit",(function(n){var a;n.preventDefault(),t.submitButton.disabled=!0,t.submitButton.classList.toggle("disabled"),
-//!   refs.iconSpinner.classList.toggle('is-hidden');
-//!   refs.iconSearch.classList.toggle('is-hidden');
-o=n.target.elements.searchQuery.value.trim(),(a=o,e.set("q",a),fetch("https://pixabay.com/api/?".concat(e)).then((function(t){if(t.ok)return t.json();throw new Error})).catch((function(t){return t}))).then((function(t){var e=t.hits;r=e,console.log(r)}))}))}();
-//# sourceMappingURL=index.ae44e805.js.map
--- index.de29427d.js
+++
@@ -1,5 +0,0 @@
-var e=refs={searchForm:document.querySelector(".search-form"),submitButton:document.querySelector(".let-search")};const t=new URLSearchParams({key:"31991210-5d8d315bab6d2995c6cf86716",q:"cats",image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(e);let o="",r=[];console.dir(e.submitButton);e.searchForm.addEventListener("submit",(s=>{var a;s.preventDefault(),e.submitButton.disabled=!0,e.submitButton.classList.toggle("disabled"),
-//!   refs.iconSpinner.classList.toggle('is-hidden');
-//!   refs.iconSearch.classList.toggle('is-hidden');
-o=s.target.elements.searchQuery.value.trim(),(a=o,t.set("q",a),fetch(`https://pixabay.com/api/?${t}`).then((e=>{if(e.ok)return e.json();throw new Error})).catch((e=>e))).then((({hits:e})=>{r=e,console.log(r)}))}));
-//# sourceMappingURL=index.de29427d.js.map
